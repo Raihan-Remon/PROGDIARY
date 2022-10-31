2cec6f67a9b80bbb8771b7c62b4d5cad9010bb64
Raihan-Remon <shamsulraihanremon@gmail.com>
2022-11-01 03:02:48 +0600

    Final Project

--- database.py
+++ database.py
@@ -1,16 +1,23 @@
 import sqlite3
 
 connection = sqlite3.connect("data.db")
+connection.row_factory = sqlite3.Row
 
 
 def create_table():
     with connection:
-        connection.execute("CREATE TABLE entries (content TEXT, date Text);")
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS entries (content TEXT, date Text);"
+        )
 
 
 def add_entry(entry_content, entry_date):
-    entries.append({"content": entry_content, "date": entry_date})
+    with connection:
+        connection.execute(
+            "INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date)
+        )
 
 
 def get_entries():
-    return entries
+    cursor = connection.execute("SELECT * FROM entries;")
+    return cursor
